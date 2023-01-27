306727ce274c03f40e49f02ba627c7b6cc27375b
Marjory Marquez <samaterra@hotmail.com>
2023-01-27 05:23:07 -0500

    readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AccountService } from './_services';
+import { Account, Role } from './_models';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'thermos';
+  account: Account;
+
+
 }
